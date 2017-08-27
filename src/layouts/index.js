e7c010ce3cd857576212c52c70b30ffa73be903c
Harrison <hmcguire13@gmail.com>
2017-08-27 10:38:41 -0400

    svg edits

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,6 +7,24 @@ import Navbar from '../components/Navbar'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '../styles/index.styl'
 
+// var last_known_scroll_position = 0;
+// var ticking = false;
+
+// function doSomething(scroll_pos) {
+//   console.log(scroll_pos)
+// }
+
+// window.addEventListener('scroll', function(e) {
+//   last_known_scroll_position = window.scrollY;
+//   if (!ticking) {
+//     window.requestAnimationFrame(function() {
+//       doSomething(last_known_scroll_position);
+//       ticking = false;
+//     });
+//   }
+//   ticking = true;
+// });
+
 const TemplateWrapper = ({ children }) =>
   <div>
     <Helmet
