ac1cf991c9d4353c06f39d9e18242aa41bb147f6
Harrison <hmcguire13@gmail.com>
2017-09-02 18:13:05 -0400

    header edits

--- src/components/Navbar/Nav.js
+++ src/components/Navbar/Nav.js
@@ -1,9 +1,45 @@
-import React from 'react'
-import Container from '../utils/Container'
+import React, { Component }  from 'react'
 
-const Nav = ({ children }) =>
-    <nav className="navbar navbar-expand-lg navbar-dark fixed-top">
-        { children }
-    </nav>
+class Nav extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isActive: false
+        }
+
+        this.toggleBG = this.toggleBG.bind(this);
+    }
+
+    toggleBG() {
+        console.log('hello')
+        this.setState(prevState => ({
+            isActive: !prevState.isActive
+        }))
+    }
+
+    componentDidMount() {
+        const _this = this
+        window.addEventListener('scroll', function(e) {
+            if(window.scrollY>25) {
+                _this.setState ({
+                    isActive: true
+                })
+            } else {
+                _this.setState ({
+                    isActive: false
+                })
+            }
+        })
+    }
+
+    render() {
+        return (
+            <nav className={'navbar navbar-expand-lg navbar-dark fixed-top ' + (this.state.isActive ? 'fill-bg' : '')}>
+                { this.props.children }
+            </nav>
+        )
+    }
+}
 
 export default Nav
--- src/components/ShowCaseVideo.js
+++ src/components/ShowCaseVideo.js
@@ -1,13 +1,18 @@
 import React, { Component } from 'react'
+import Responsive from 'react-responsive';
 import oceanVideoMP4 from '../videos/ocean.mp4'
 import oceanVideoWEBM from '../videos/ocean.webm'
 
+const DeskTab = ({ children }) => <Responsive minWidth={736} children={children} />;
+
 const ShowCaseVideo = () =>
-    <div className="tv">
-        <video preload="auto" className="showcase-video" autoPlay loop muted>
-            <source src={oceanVideoMP4} type="video/mp4" />
-            <source src={oceanVideoWEBM} type="video/webm" />
-        </video>
-    </div>
+    <DeskTab>
+        <div className="tv">
+            <video preload="auto" className="showcase-video" autoPlay loop muted>
+                <source src={oceanVideoMP4} type="video/mp4" />
+                <source src={oceanVideoWEBM} type="video/webm" />
+            </video>
+        </div>
+    </DeskTab>
 
 export default ShowCaseVideo
