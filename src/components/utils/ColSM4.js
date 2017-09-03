5f41c4ec94824ab47ef08df6d6e75385a34eac63
Harrison <hmcguire13@gmail.com>
2017-09-03 07:37:56 -0400

    edits

---
+++ src/components/utils/ColSM4.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const ColSM4 = ({ children }) =>
+    <div className="col-sm-4">
+        { children }
+    </div>
+
+export default ColSM4
---
+++ src/components/utils/Row.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+const Row = ({ children }) =>
+    <div className="row">
+        { children }
+    </div>
+
+export default Row
