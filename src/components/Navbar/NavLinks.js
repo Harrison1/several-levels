85427fd86cda13e757a54bf8a2cbf0d49f30492a
Harrison <hmcguire13@gmail.com>
2017-09-04 12:42:13 -0400

    edits

--- src/components/Navbar/NavLinks.js
+++ src/components/Navbar/NavLinks.js
@@ -5,8 +5,8 @@ const NavLinks = () =>
     <div className="collapse navbar-collapse justify-content-end">
         <div className="navbar-nav">
             <a className="nav-item nav-link text-white" href="#services">Services</a>
-            <a className="nav-item nav-link text-white" href="https://blog.severallevels.io">Blog</a>
             <a className="nav-item nav-link text-white" href="#contact">Contact</a>
+            <a className="nav-item nav-link text-white" href="#around">Around the Web</a>
         </div>
     </div>
 
