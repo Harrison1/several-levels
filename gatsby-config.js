f6cbf9fff082fe930379f1f751eb15cfc801443a
Harrison <hmcguire13@gmail.com>
2017-10-03 05:52:50 -0400

    gatsby and site update

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,10 @@ module.exports = {
   siteMetadata: {
     title: `several levels`,
   },
-  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-stylus`],
+  plugins: [`gatsby-plugin-react-helmet`, `gatsby-plugin-stylus`, {
+    resolve: `gatsby-plugin-google-analytics`,
+    options: {
+      trackingId: `UA-73708584-1`,
+    },
+  }],
 }
